17fece399306b051c368eae47038762b9a57b34e
Microshaoft <microshaoft@msn.com>
2020-02-19 11:46:01 +0800

    .net core 3.1 + Microsoft.AspNet.WebApi.Client v5.2.7 + ConsoleClient Test

---
+++ HttpClient.NET.Core.ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+
+namespace GL.Multipart.Client
+{
+    class Program
+    {
+        const string serviceBaseAddress = "http://localhost:9000/";
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var requestBaseAddress = serviceBaseAddress;
+            requestBaseAddress = "http://localhost:9000/";
+            client.BaseAddress = new Uri(requestBaseAddress);
+            var batchrequest = CreateBatch(client, "api/asyncbatch");
+            var response = client.SendAsync(batchrequest).Result;
+            var ss = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(ss);
+            Console.ReadLine();
+        }
+        private static HttpRequestMessage CreateBatch(HttpClient client, string endpoint)
+        {
+            HttpRequestMessage get1Values = new HttpRequestMessage(HttpMethod.Get, client.BaseAddress + "get1?id=11111");
+            HttpRequestMessage get2Values = new HttpRequestMessage(HttpMethod.Get, client.BaseAddress + "post1?id=2222")
+            {
+                //Content = new StringContent("id=2222")
+            };
+            
+
+            HttpMessageContent get1Content = new HttpMessageContent(get1Values);
+            HttpMessageContent get2Content = new HttpMessageContent(get2Values);
+            
+
+            MultipartContent content = new MultipartContent("mixed", "batch_" + Guid.NewGuid().ToString());
+            content.Add(get1Content);
+            content.Add(get2Content);
+            
+
+            HttpRequestMessage batchRequest = new HttpRequestMessage(HttpMethod.Post, endpoint);
+            //Associate the content with the message
+            batchRequest.Content = content;
+
+            return batchRequest;
+
+        }
+    }
+}
--- WebAPIBatchRequestsHandler/ValuesController.cs
+++ WebAPIBatchRequestsHandler/ValuesController.cs
@@ -7,64 +7,107 @@
     using WebApi.OutputCache.V2;
     using WebApi.OutputCache.V2.TimeAttributes;
 
+    public class Aa
+    {
+        public string F1;
+        public int F2;
+
+    }
+
     public class ValuesController : ApiController
     {
         [Route("get1")]
-        // GET api/values 
-        [
-            CacheOutput
-                (
-                    ClientTimeSpan = 10
-                    , ServerTimeSpan = 10
-                    , ExcludeQueryStringFromCacheKey = false
-                )
-        ]
         [HttpGet]
-        //[CacheOutputUntilToday(23, 59, 59)]
-        public async Task<string> Get1
+        public Aa[] Process
                             (
-                                string data
+                                [FromUri]
+                                int id
                             )
         {
-            await Task.Delay(1000);
-            return string.Format("I am Get1 ({0})! @ [{1}]", data, DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
+            return new Aa[] 
+            
+            { 
+                new Aa
+                {
+                    F1= "aaaa"
+                    , F2 = id
+                }
+            
+            };
         }
-
-        [Route("get2")]
+        [Route("post1")]
         [HttpGet]
-        [CacheOutputUntilToday(23, 59, 59)]
-        // GET api/values 
-        public async Task<string> Get2()
-        {
-            await Task.Delay(1000);
-            return string.Format("I am Get2 ! @ [{0}]", DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
-        }
-
-        [Route("get3")]
-        // GET api/values
-        [HttpGet]
-        [CacheOutputUntilToday(23, 59, 59)]
-        public async Task<string> Get3()
-        {
-            await Task.Delay(1000);
-            return string.Format("I am Get3 ! @ [{0}]", DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
-        }
-
-
-        [Route("Post1")]
-        // GET api/values
-        [HttpPost]
-        //post CacheOutput 无效 
-        //[CacheOutput(ClientTimeSpan = 50, ServerTimeSpan = 50, ExcludeQueryStringFromCacheKey = false)]
-        [CacheOutputUntilToday(23,59,59)]
-        public async Task<string> Post1
+        public Aa[] Post
                         (
                             [FromBody]
-                            JObject data
+                            int id
                         )
         {
-            await Task.Delay(1000);
-            return string.Format("I am Post1({0}) ! @ [{1}]", data ,DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
+            return Process(id);
         }
+
+        
+        //[Route("get1")]
+        //// GET api/values 
+        ////[
+        ////    CacheOutput
+        ////        (
+        ////            ClientTimeSpan = 10
+        ////            , ServerTimeSpan = 10
+        ////            , ExcludeQueryStringFromCacheKey = false
+        ////        )
+        ////]
+        //[HttpGet]
+        //[HttpPost]
+        ////[CacheOutputUntilToday(23, 59, 59)]
+        //public async Task<string> Process
+        //                    (
+        //                        [FromUri]    
+        //                        string q1
+        //                        , 
+        //                        [FromBody]
+        //                        string b1 = null
+        //                    )
+        //{
+        //    await Task.Delay(1000);
+        //    return string.Format($"I am Get1 ({q1}{b1})! @ [{DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff")}]");
+        //}
+
+        //[Route("get2")]
+        //[HttpGet]
+        //[CacheOutputUntilToday(23, 59, 59)]
+        //// GET api/values 
+        //public async Task<string> Get2()
+        //{
+        //    await Task.Delay(1000);
+        //    return string.Format("I am Get2 ! @ [{0}]", DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
+        //}
+
+        //[Route("get3")]
+        //// GET api/values
+        //[HttpGet]
+        //[CacheOutputUntilToday(23, 59, 59)]
+        //public async Task<string> Get3()
+        //{
+        //    await Task.Delay(1000);
+        //    return string.Format("I am Get3 ! @ [{0}]", DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
+        //}
+
+
+        //[Route("Post1")]
+        //// GET api/values
+        //[HttpPost]
+        ////post CacheOutput 无效 
+        ////[CacheOutput(ClientTimeSpan = 50, ServerTimeSpan = 50, ExcludeQueryStringFromCacheKey = false)]
+        //[CacheOutputUntilToday(23,59,59)]
+        //public async Task<string> Post1
+        //                (
+        //                    [FromBody]
+        //                    JObject data
+        //                )
+        //{
+        //    await Task.Delay(1000);
+        //    return string.Format("I am Post1({0}) ! @ [{1}]", data ,DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss.fff"));
+        //}
     }
 }
